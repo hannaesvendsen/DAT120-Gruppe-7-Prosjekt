fef82897ad1101fe493add78c774739f4310d753
Odin-Troli19 <79744512+Odin-Troli19@users.noreply.github.com>
2022-11-02 10:54:51 +0100

    Update projekt9.py

--- projekt9.py
+++ projekt9.py
@@ -10,23 +10,36 @@ varighet
     
 import datetime as dt
 
-x = dt.datetime.now()
 
 class Avtale:
-    def __init__(self, tittel, sted, start, varighet):
-      self.tittel = tittel
-      self.sted = sted
-      self.start = x
-      self.varighet = varighet
-      
-      
-    def __str__(self):
-        return f"tittel: {self.tittel}, Sted: {self.sted}, starttidspunkt: {self.start}, Varighet: {self.varighet}min"
+  def __init__(self, tittel, sted, start, varighet):
+    self.tittel = tittel
+    self.sted = sted
+    self.start = start
+    self.varighet = varighet
+    
+    
+  def __str__(self):
+    return f"tittel: {self.tittel}, Sted: {self.sted}, starttidspunkt: {self.start}, Varighet: {self.varighet}min" 
 
 
-
-    def __repr__(self):
-        return f"tittel: {self.tittel}, Sted: {self.sted}, starttidspunkt: {self.start}, Varighet: {self.varighet}min"
+def ny_avtale():
+    Avtalen = None
+    while not Avtalen:
+        try:        
+            tittel_1 = input("Angi tittel til avtalen : ")
+            sted_1 = input("Angi stedet til avtalen : ")
+            start_tidspunkt_1 = input("Angi dato og tidspunkt på formatet: dd:mm:yyyy tt:mm ")
+            start_tidspunktdaytime = dt.datetime.strptime(start_tidspunkt_1, "%d:%m:%Y %H:%M")
+            varighet_1 = int(input("Angi varighte i minutter :"))
+            Avtalen=Avtale(tittel_1, sted_1, start_tidspunktdaytime, varighet_1)
+        except Exception as error:
+                print(error)
+    return Avtalen
+    
+     
+a=ny_avtale()
+print (a)
       
       
 def avtale_funksjon(ordliste, overskrift=""):
